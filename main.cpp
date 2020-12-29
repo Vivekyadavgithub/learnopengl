d3c329e36f2878445325bcab09b3ace081085c67
vivek393 <59399015+vivek393@users.noreply.github.com>
2020-12-29 08:53:26 +0530

    texture

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 int main()
 {
 	GLFWwindow* window;
@@ -23,41 +26,100 @@ int main()
 	if (glewInit() != GLEW_OK) {
 		std::cout << "Error\n";
 	}
+	
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
 
-	float position[] = {
-		0.5f, -0.5f,
-		-0.5f, -0.5f,
-		0.5f, 0.0f
+	float vertices[] = {
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 
+	float position[] = {
+	 0.5f/2.0f,  0.5f/2.0f, 0.0f/2.0f,  // top right
+	 0.5f/2.0f, -0.5f/2.0f, 0.0f/2.0f,  // bottom right
+	-0.5f/2.0f, -0.5f/2.0f, 0.0f/2.0f,  // bottom left
+	-0.5f/2.0f,  0.5f/2.0f, 0.0f,   // top left 
+	 1.0f     ,  1.0f     , 1.0f
+	};
+
+	unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3,    // second triangle
+	};
+
+	float texCoords[] = {
+			0.0f, 0.0f,  // lower-left corner  
+			1.0f, 0.0f,  // lower-right corner
+			0.5f, 1.0f   // top-center corner
+	};
+	std::cout << "heo\n";
 	const char* vertexShaderSource = 
 		"#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
+		"layout(location = 1) in vec3 aColor;\n"
+	    "layout(location = 2) in vec2 aTexCoord;\n"
+		"out vec3 ourColor;\n"
+	    "out vec2 TexCoord;\n"
 		"void main()\n"
 		"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+			"gl_Position = vec4(aPos, 1.0);\n"
+			"ourColor = aColor;\n"
+			"TexCoord = aTexCoord;\n"
 		"}\0";
 
 	const char* fragmentShaderSource =
 		"#version 330 core\n"
 		"out vec4 FragColor;\n"
+		"in vec3 ourColor;\n"
+		"in vec2 TexCoord;\n"
+		"uniform sampler2D ourTexture;\n"
 		"void main()\n"
 		"{\n"
-		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"   FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);\n"
 		"}\n";
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	//glBuffer
-	glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), position, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 4*8*sizeof(float), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+	//glVertexAttribPointer(0, 8, GL_FLOAT, GL_FALSE, 8 * sizeof(unsigned), 0);
+	//glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3,  GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	unsigned int index_buffer;
+	glGenBuffers(1, &index_buffer);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned), indices, GL_STATIC_DRAW);
+	
+	
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(VAO);
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
 
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
@@ -85,23 +147,58 @@ int main()
 	glUseProgram(shaderProgram);*/
 	// 3. now draw the object 
 	//someOpenGLFunctionThatDrawsOurTriangle();
+	//float timeValue = glfwGetTime();
+	//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+	//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+	//glUseProgram(shaderProgram);
+	//glUseProgram(shaderProgram);
+	//float timeValue = glfwGetTime();
+	//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+	//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+	
 
+	//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+	//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
 		//processInput(window);
-		glClear(GL_COLOR_BUFFER_BIT); 
+		//glClear(GL_COLOR_BUFFER_BIT); 
 		// rendering commands here
 		//glUseProgram(shaderProgram);
 		//glBindVertexArray(buffer);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+		/*glUseProgram(shaderProgram);
+		float timeValue = glfwGetTime();
+		float greenValue = sin(timeValue) / 2.0f + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);*/
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// be sure to activate the shader before any calls to glUniform
+		glUseProgram(shaderProgram);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// update shader uniform
+		//float timeValue = glfwGetTime();
+		//float greenValue = sin(timeValue) / 2.0f + 0.5f;
+		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES,9, GL_UNSIGNED_INT, nullptr);
 		//glDrawElements(GL_TRIANGLES, 3, GL_FLOAT, nullptr);
 		glfwSwapBuffers(window);
-			// check and call events and swap the buffers
+		// check and call events and swap the buffers
 	    glfwPollEvents();
 		
 	}
+
+	stbi_image_free(data);
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &index_buffer);
+	glDeleteProgram(shaderProgram);
 	glfwTerminate();
-    
 }
